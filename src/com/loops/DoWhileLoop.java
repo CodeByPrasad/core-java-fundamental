f00bbb6fc2acc2083d41327bb29347b597dc5719
prasad <prasad2082001@gmail.com>
2025-05-08 11:24:58 +0530

    Loops in java

---
+++ src/com/loops/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args){
+
+        int i = 5;
+        do {
+            System.out.println(i);
+            i--;
+        } while (i>3);
+
+    }
+}
